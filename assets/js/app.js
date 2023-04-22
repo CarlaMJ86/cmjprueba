fe0975644d2af51893fd3bb1bad61cf35ce2149c
Carla Marquez Jara <carla.marquezjara@gmail.com>
2023-04-22 13:16:26 -0400

    Mas avances cards y columnas

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,5 +25,46 @@ $(document).ready(function () {
           }
         });
       });
-      
+      $(".card").hover(
+        function () {
+          var cardChange = $(this); // caching $(this)
+          cardChange.css("color", "black");
+        },
+        function () {
+          var cardChange = $(this); // caching $(this)
+          cardChange.css("color", "black");
+        }
+      );
+    
+      $(".card1")
+        .mouseover(function () {
+          $(".card1txt").css("color", "#000000");
+        })
+        .mouseleave(function () {
+          $(".card1txt").css("color", "#ffffff");
+        });
+    
+      $(".card2")
+        .mouseover(function () {
+          $(".card2txt").css("color", "#000000");
+        })
+        .mouseleave(function () {
+          $(".card2txt").css("color", "#ffffff");
+        });
+    
+      $(".card3")
+        .mouseover(function () {
+          $(".card3txt").css("color", "#000000");
+        })
+        .mouseleave(function () {
+          $(".card3txt").css("color", "#ffffff");
+        });
+    
+      $(".card4")
+        .mouseover(function () {
+          $(".card4txt").css("color", "#000000");
+        })
+        .mouseleave(function () {
+          $(".card4txt").css("color", "#ffffff");
+        });
 });
